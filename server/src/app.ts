ea54279fdade1aa034b3e45fd47b4189b3c73c1e
Matthew Walters <matt@webflame.io>
2024-03-02 16:04:41 -0800

    WIP

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { fooTime } from "./foo";
 
 const app = express();
 const port = 4000;
@@ -7,7 +8,7 @@ app.use(express.json()); // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
 
 app.get("/", (_req, res) => {
-  res.send("Hello, World!");
+  res.send(`Hello, World! ${fooTime()}`);
 });
 
 app.get("/time", (_req, res) => {
